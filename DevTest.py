640599c1130fb1a6ddcf0b861a6a6be46e10c65f
yousufmca05-netizen <yousufmca05@gmail.com>
2025-12-18 08:13:11 -0500

    Create DevTest.py

---
+++ DevTest.py
@@ -0,0 +1,8 @@
+# Print a message
+print("Hello, World!")
+
+# Take user input
+name = input("Enter your name: ")
+
+# Display a personalized message
+print(f"Hello, {name}! Welcome to Python programming.")
